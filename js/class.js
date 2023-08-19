783253647d63de0e984f78d4dc80e1d0444071ab
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-19 18:33:14 +0600

    Access modifier

--- js/class.js
+++ js/class.js
@@ -11,10 +11,52 @@ var Player = /** @class */ (function () {
     return Player;
 }());
 var farhad = new Player("Mohammad Farhad", 19, "Bangladesh");
-var sakib = new Player("Mohammad Sakib", 20, "Bangladesh");
 var irfan = new Player("Mohammad Irfan", 15, "Bangladesh");
 // farhad.play();
 // use class as data type
 var players = [];
-players.push(sakib);
+players.push(irfan);
 players.push(farhad);
+//-------------- access modifier --------------
+var Student = /** @class */ (function () {
+    function Student(id, name, age) {
+        var _this = this;
+        this.message = function () {
+            console.log("ID ".concat(_this.id, " is for ").concat(_this.name, " who is ").concat(_this.age, " years old "));
+        };
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    return Student;
+}());
+var farhadStudent = new Student(101, "Mohammad Farhad", 19);
+// public: readable and re-assignable
+// console.log(farhadStudent.id);
+farhadStudent.id = 50;
+// readonly: readable but not re-assignable
+// console.log(farhadStudent.name);
+// farhadStudent.name = 'Mohammad Farhad Attari'
+// private: cannot readable and assignable
+// console.log(farhadStudent.age);
+// farhadStudent.age = 17;
+// ------------------ class in easy way ----------
+var Programmer = /** @class */ (function () {
+    function Programmer(name, tech, salary, skills) {
+        var _this = this;
+        this.name = name;
+        this.tech = tech;
+        this.salary = salary;
+        this.skills = skills;
+        this.about = function () {
+            console.log("".concat(_this.name, " is ").concat(_this.tech, " with skills ").concat(_this.skills.join(", "), ". Expected salary is ").concat(_this.salary));
+        };
+    }
+    return Programmer;
+}());
+var proFarhad = new Programmer("Mohammad Farhad", "Web Developer", 15000, [
+    "JS",
+    "TS",
+    "React",
+]);
+// proFarhad.about();
--- ts/class.ts
+++ ts/class.ts
@@ -15,11 +15,67 @@ class Player {
 }
 
 const farhad = new Player("Mohammad Farhad", 19, "Bangladesh");
-const sakib = new Player("Mohammad Sakib", 20, "Bangladesh");
 const irfan = new Player("Mohammad Irfan", 15, "Bangladesh");
 // farhad.play();
 
 // use class as data type
 let players: Player[] = [];
-players.push(sakib);
+players.push(irfan);
 players.push(farhad);
+
+//-------------- access modifier --------------
+class Student {
+  public id: number | string;
+  readonly name: string;
+  private age: number;
+  constructor(id: string | number, name: string, age: number) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  message = () => {
+    console.log(
+      `ID ${this.id} is for ${this.name} who is ${this.age} years old `
+    );
+  };
+}
+
+const farhadStudent = new Student(101, "Mohammad Farhad", 19);
+// public: readable and re-assignable
+// console.log(farhadStudent.id);
+farhadStudent.id = 50;
+
+// readonly: readable but not re-assignable
+// console.log(farhadStudent.name);
+// farhadStudent.name = 'Mohammad Farhad Attari'
+
+// private: cannot readable and assignable
+// console.log(farhadStudent.age);
+// farhadStudent.age = 17;
+
+// ------------------ class in easy way ----------
+class Programmer {
+  constructor(
+    readonly name: string,
+    readonly tech: string,
+    private salary: number,
+    public skills: string[]
+  ) {}
+
+  about = () => {
+    console.log(
+      `${this.name} is ${this.tech} with skills ${this.skills.join(
+        ", "
+      )}. Expected salary is ${this.salary}`
+    );
+  };
+}
+
+const proFarhad = new Programmer("Mohammad Farhad", "Web Developer", 15000, [
+  "JS",
+  "TS",
+  "React",
+]);
+
+// proFarhad.about();
